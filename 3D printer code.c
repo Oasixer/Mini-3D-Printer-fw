9a4a8a3368909f48189e0206389b58034dcbea8c
Kaelan Moffett-Steinke <kaelan.ms@gmail.com>
2018-11-08 22:18:35 -0500

    added colorScan

--- 3D printer code.c
+++ 3D printer code.c
@@ -99,7 +99,7 @@ int scanPaper(){
 	for (int x=0;x<6;x++){
 		for(int y=0;y<6;y++){
 			moveXYMM(x*15,y*15,false);
-			int height=scanPaper();
+				int height=scanPaper();
 			if (height>maxLayer)
 				maxLayer=height;
 			for (int lay=0;lay<height;lay++)
@@ -109,7 +109,20 @@ int scanPaper(){
 }
 
 int scanColour(){
-	//NOT DONEEEEEEEEEEEEEEEEEEEEEEEEEEE
+	SensorType[S1]=sensorEV3_Color;
+	wait1Msec(50);
+	SensorMode[S1]=modeEV3Color_Color;
+	wait1Msec(50);
+
+	int values[3]={0}; //store 3 colour values
+	do{ //loop until all 3 colour values are the same (aka make sure you dont read the wrong colour)
+		values[0]=SensorValue[S1];
+		wait1Msec(50);
+		values[1]=SensorValue[S1];
+		wait1Msec(50);
+		values[2]=SensorValue[S1];
+		wait1Msec(50);
+	} while (!(values[0]==values[1]&&values[0]==values[2]))
 }
 
 //done
