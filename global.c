32cb508dd2345efdc2dc2afb23063e04c5f3480c
Lyndon Tang <44735835+le2tang@users.noreply.github.com>
2018-11-14 15:34:06 -0500

    Working on correct data types

--- global.c
+++ global.c
@@ -3,8 +3,8 @@ const int VELOCITY = 25; //mm/s 200/8
 const float TOLERANCE = 0.5;
 const tMotor motorY = motorA, motorX = motorD;
 const tMotor motorZ1 = motorB, motorZ2 = motorC;
-const tSensors COLOR_SENSOR = S1, TOUCH_Z = S2; TOUCH_Y = S3;
-const tSensors I2C_PORT = S4;
+const tSensor COLOR_SENSOR = S1, TOUCH_Z = S2; TOUCH_Y = S3;
+const tSensor I2C_PORT = S4;
 float deltaX, deltaY, powerX, powerY, yTar, xTar, xCurrent, yCurrent;
 const float ENC_TO_MM=(float)24/360;
 const float MM_TO_ENC=15;
