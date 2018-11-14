c90154bc8785a90b3b5c817ace613406585fbbf4
le2tang <le2tang@edu.uwaterloo.ca>
2018-11-14 17:04:04 -0500

    Fixed data types

--- global.c
+++ global.c
@@ -1,10 +1,14 @@
 // by Zachariah, Kaelan, Lyndon, Daddy Parth
 const int VELOCITY = 25; //mm/s 200/8
 const float TOLERANCE = 0.5;
-const tMotor motorY = motorA, motorX = motorD;
-const tMotor motorZ1 = motorB, motorZ2 = motorC;
-const tSensor COLOR_SENSOR = S1, TOUCH_Z = S2; TOUCH_Y = S3;
-const tSensor I2C_PORT = S4;
+const tMotor motorY = motorA;
+const tMotor motorX = motorD;
+const tMotor motorZ1 = motorB;
+const tMotor motorZ2 = motorC;
+const tSensors COLOR_SENSOR = S1;
+const tSensors TOUCH_Z = S2;
+const tSensors TOUCH_Y = S3;
+const tSensors I2C_PORT = S4;
 float deltaX, deltaY, powerX, powerY, yTar, xTar, xCurrent, yCurrent;
 const float ENC_TO_MM=(float)24/360;
 const float MM_TO_ENC=15;
