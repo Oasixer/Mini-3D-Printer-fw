ef24ffeb0d9e840f7fcfb4b13309bd84f630d6d4
Lyndon Tang <44735835+le2tang@users.noreply.github.com>
2018-11-14 10:57:53 -0500

    Modified moveXY function to give non-zero power
    Added display() function to show vector components on screen for troubleshooting

--- move.c
+++ move.c
@@ -5,44 +5,63 @@ float motorPower (float velocity)
 {
 	const float slope = 1, offset = 0, exponent = 1;
 	if (velocity < 0)
-		return -slope*pow(abs(velocity), exponent) + offset;
+		return -slope * pow(abs(velocity), exponent) + offset;
 	else
-		return slope*pow(abs(velocity), exponent) + offset;
+		return slope * pow(abs(velocity), exponent) + offset;
 }
 
 void moveXY (float xTarget, float yTarget)
 {
-	xCur=nMotorEncoder[motorX]*ENC_TO_MM;
-	yCur=nMotorEncoder[motorY]*ENC_TO_MM;
-	xTar=xTarget;
-	yTar=yTarget;
-	deX = xTar-xCur;
-	deY = yTar-yCur;
-	if (abs(deX)> TOLERANCE)
+	float xCurrent = nMotorEncoder[motorX] * ENC_TO_MM;
+	float yCurrent = nMotorEncoder[motorY]*ENC_TO_MM;
+	
+	float deltaX = xTarget - xCurrent;
+	float deltaY = yTarget - yCurrent;
+	
+	float deltaV = sqrt(deltaX*deltaX + deltaY*deltaY);
+	
+	display(0, xCurrent, yCurrent);
+	display(1, deltaX, deltaY);
+	
+	// xCur=nMotorEncoder[motorX]*ENC_TO_MM;
+	// yCur=nMotorEncoder[motorY]*ENC_TO_MM;
+	// xTar=xTarget;
+	// yTar=yTarget;
+	// deX = xTar-xCur;
+	// deY = yTar-yCur;
+	
+	if (abs(deltaX) > TOLERANCE)
 	{
-		moX = motorPower(VELOCITY* deX /sqrt(deX*deX+deY*deY));
-		step=1.5;//DEBUG
-		if (deX<0)
-			moX*=-1;
-		motor[motorX] = moX;
+		float powerX = motorPower((float) VELOCITY * deltaX / deltaV);
+		step = 1.5;//DEBUG
+		if (deltaX < 0)
+			powerX *= -1;
+		motor[motorX] = powerX;
 	}
-	if (abs(deY)> TOLERANCE)
+	if (abs(deltaY) > TOLERANCE)
 	{
 		step=1.5;//DEBUG
-		moY=motorPower(VELOCITY*deY/sqrt(deX*deX+deY*deY));
-		if (deY<0)
-			moY*=-1;
-		motor[motorY] = moY;
+		float powerY = motorPower((float) VELOCITY * deltaY / deltaV);
+		
+		moY = powerY
+		
+		if (deltaY < 0)
+			powerY *= -1;
+		motor[motorY] = powerY;
 	}
-	while (abs(xTar - xCur) > TOLERANCE || abs(yTar - yCur) > TOLERANCE){
-		xCur=nMotorEncoder[motorX]*ENC_TO_MM;
-		yCur=nMotorEncoder[motorY]*ENC_TO_MM;
-		if (abs(xTar - xCur)<TOLERANCE)
+	
+	display(3, powerX, powerY);
+	
+	while (!(abs(xTarget - xCurrent) < TOLERANCE && abs(yTarget - yCurrent) > TOLERANCE))
+	{
+		xCurrent = nMotorEncoder[motorX] * ENC_TO_MM;
+		yCurrent = nMotorEncoder[motorY] * ENC_TO_MM;
+		if (abs(xTarget - xCurrent) < TOLERANCE)
 		{
 			motor[motorX] = 0;
 			moX=0;
 		}
-		if (abs(yTarget - yCur)<TOLERANCE)
+		if (abs(yTarget - yCurrent) < TOLERANCE)
 		{
 			motor[motorY] = 0;
 			moY=0;
@@ -52,6 +71,11 @@ void moveXY (float xTarget, float yTarget)
 	moX=0;
 	motor[motorY]=0;
 	moY=0;
+}
+
+void display(int line, float x, float y)
+{
+	displayString(line, "(%f, %f)", x, y);
 }
 
 void moveZ (float zTarget)
