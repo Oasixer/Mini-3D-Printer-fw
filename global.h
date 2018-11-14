1afbb0bcfc5c90bc77685a3dd8818ca585a4d40d
Lyndon Tang <44735835+le2tang@users.noreply.github.com>
2018-11-14 15:39:08 -0500

    Added header file
    If no task main(), must use header

---
+++ global.h
@@ -0,0 +1,18 @@
+// by Zachariah, Kaelan, Lyndon, Daddy Parth
+const int VELOCITY = 25; //mm/s 200/8
+const float TOLERANCE = 0.5;
+const tMotor motorY = motorA;
+const tMotor motorX = 3;
+const tMotor motorZ1 = motorB;
+const tMotor motorZ2 = motorC;
+const tSensors COLOR_SENSOR = S1
+const tSensors TOUCH_Z = S2;
+const tSensors TOUCH_Y = S3;
+const tSensors I2C_PORT = S4;
+float deltaX, deltaY, powerX, powerY, yTar, xTar, xCurrent, yCurrent;
+const float ENC_TO_MM=(float)24/360;
+const float MM_TO_ENC=15;
+const float LAYERHEIGHT=0.5;
+const float MAXLAYER=5*LAYERHEIGHT;
+float step;//for debugging
+int cubes[5][5];
