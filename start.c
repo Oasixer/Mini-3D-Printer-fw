3517be0d8346ad3f39a8aea875d742e74e734d55
Lyndon Tang <44735835+le2tang@users.noreply.github.com>
2018-11-16 16:50:05 -0500

    new zero stuff

--- start.c
+++ start.c
@@ -1,53 +1,65 @@
 #include "global.c"
 #include "move.c"
 #include "buttons.c"
-#include "math.c"
 
 #ifndef START_C
 #define START_C
+
+float max(float number1, float number2)
+{
+	if (number1 > number2)
+		return number1;
+	return number2;
+}
 
 void configureI2C (tSensors port){ // make sure to call function to set up servo
 	SensorType[port] = sensorI2CCustom9V;
 }
 
 void zero(tSensors touchY, tSensors touchZ, int velocity)
-{	
-	powerX = -velocity;
-	powerY = -velocity;
-	powerZ = max(-2*velocity, -100);
-	
+{
+	float powerX = -velocity;
+	float powerY = -velocity;
+	float powerZ = max(-2*velocity, -100);
+
+	// zero x-axis
+	motor[motorX] = 7;
+	motor[motorZ1] = 100;
+	motor[motorZ2] = 100;
+	wait1Msec(1000);
+	motor[motorZ1] = motor[motorZ2] = 0;
+	wait1Msec(25000);
+	motor[motorX] = 0;
+
 	// zero y-axis
 	motor[motorY] = powerY;
 	while(SensorValue[touchY] == 0) {}
 	motor[motorY] = 0;
 	nMotorEncoder[motorY] = 0;
-	
+
 	// zero z-axis
 	motor[motorZ1] = motor[motorZ2] = powerZ;
 	while(SensorValue[touchZ] == 0) {}
 	motor[motorZ1] = motor[motorZ2] = 0;
 	nMotorEncoder[motorZ1] = 0;
 	nMotorEncoder[motorZ2] = 0;
-	
-	// zero x-axis
-	
-	moveXY(1, 1);
-	moveZ(1);
-	
-	// zero y-axis
-	motor[motorY] = powerY / 2;
+
+	moveXY(0, 20);
+	moveZ(20);
+
+	// zero y-axis at quarter speed
+	motor[motorY] = powerY / 4;
 	while(SensorValue[touchY] == 0) {}
 	motor[motorY] = 0;
 	nMotorEncoder[motorY] = 0;
-	
-	// zero z-axis at half speed
+
+	// zero z-axis at quarter speed
 	motor[motorZ1] = motor[motorZ2] = powerZ / 2;
 	while(SensorValue[touchZ] == 0) {}
 	motor[motorZ1] = motor[motorZ2] = 0;
 	nMotorEncoder[motorZ1] = 0;
 	nMotorEncoder[motorZ2] = 0;
-	
-	//zero x-axis
+
 }
 
 int scanColour()
