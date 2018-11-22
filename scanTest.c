1cfe11a5b39bd26128785d50048811bbd5baa58d
Oasixer <kaelan.ms@gmail.com>
2018-11-22 17:39:16 -0500

    titles

---
+++ scanTest.c
@@ -0,0 +1,16 @@
+/*
+
+Group 414
+3D Printer
+Scan Test
+Nov 16, 2018
+
+*/
+
+#include "start.c"
+#include "move.c"
+
+task main()
+{
+	scanPaper();
+}
---
+++ zeroTest.c
@@ -0,0 +1,17 @@
+/*
+
+Group 414
+3D Printer
+Zero Test
+Nov 16, 2018
+
+*/
+
+#include "start.c"
+#include "move.c"
+
+task main()
+{
+	waitForEnter();
+	zero();
+}
