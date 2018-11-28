ab831080d021663e473eb38b12d0c202a0bfc62e
Oasixer <kaelan.ms@gmail.com>
2018-11-27 21:05:22 -0500

    Oh yeah also cleaned up this

--- print.c
+++ print.c
@@ -4,38 +4,11 @@
 #ifndef PRINT_C
 #define PRINT_C
 
-void printSquare (float leftx, float boty){
-	/*
-	moveXY(leftx,boty);
-	extrude(true);
-	wait1Msec(300);
-	moveXY(leftx+15,boty);
-	moveXY(leftx+15,boty+15);
-	moveXY(leftx,boty+15);
-
-	int xTarget = 0, yTarget = 1;
-	for (int pattern = 0; pattern < 7; pattern++)
-	{
-		for (int count = 0; count < 4; count++)
-		{
-			if (count == 0 || count == 3)
-				xTarget = 1;
-			else
-				// Make pbv variable to make size of square adjustable
-				xTarget = 14;
-
-				if (count%2 == 0)
-					yTarget++;
-
-			moveXY(leftx+xTarget, boty+yTarget);
-		}
-	}
-	*/
-
-	moveXY(leftx,boty);
-	extrude(true);
-  wait1Msec(900);
-	moveXY(leftx+14,boty);
+void printSquare (float leftx, float boty){ //prints one square at the given coordinates
+	moveXY(leftx,boty); //move to starting coordinates
+	extrude(true); //start extruding
+  wait1Msec(900); //wait for the filament to start coming out
+	moveXY(leftx+14,boty); //start tracing out the square using the move commands
 	moveXY(leftx+14,boty+13);
 	moveXY(leftx,boty+13);
 	moveXY(leftx,boty+0.35);
@@ -63,18 +36,17 @@ void printSquare (float leftx, float boty){
 	moveXY(leftx+12,boty+10);
 	moveXY(leftx+2,boty+10);
 	moveXY(leftx+2,boty+11);
-	extrude(false);
+	extrude(false); //stop extruding. this is called before the last movement to account for the lag of the extruder 
 	moveXY(leftx+12,boty+11);
-
 }
 
-void runPrint (){
-	for (int layer = 1; layer < MAXLAYER; layer++){
-		moveZ(layer*LAYERHEIGHT);
-		for (int x = 0; x < 5; x++){
-			for (int y = 0; y < 5; y++){
-				if ((float)(cubes[x][y])*(CUBEHEIGHT/LAYERHEIGHT) >= layer)
-					printSquare((float)x*15.0, (float)y*15.0);
+void runPrint (){ //prints the entire array of cubes
+	for (int layer = 1; layer < MAXLAYER; layer++){ //loop through every layer in the print
+		moveZ(layer*LAYERHEIGHT); //move to the height of the current layer
+		for (int x = 0; x < 5; x++){ //loop through the columns
+			for (int y = 0; y < 5; y++){ //loop through the rows
+				if ((float)(cubes[x][y])*(CUBEHEIGHT/LAYERHEIGHT) >= layer) //check if the data includes a square at the current x,y and layer 
+					printSquare((float)x*15.0, (float)y*15.0); //print a square
 			}
 		}
 	}
